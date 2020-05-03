8b7b13cd726170b98bab9d9ec254e981bbc4ff19
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-04 08:21:42 +0900

    feat: Added URL dispacher on dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home_view(request):
+    """home view
+    """
+    return HttpResponse('<html>Hello</html>')
