27e453ad0cc860a78665c7ea003d3330e48f2347
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-07 05:03:04 +0900

    feat: add require login

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home_view(request):
     """home view
     """
--- src/money-boy/settings.py
+++ src/money-boy/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'dashboard:home'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
