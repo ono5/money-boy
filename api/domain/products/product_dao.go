2175f983e19cc7ee586ed7fdafddf5ee25daeba1
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-05-25 13:32:23 +0900

    feat: Added Save method with gorm

--- api/domain/products/product_dao.go
+++ api/domain/products/product_dao.go
@@ -4,37 +4,18 @@ package products
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/ono5/money-boy/api/datasources/mysql/products_db"
 	"github.com/ono5/money-boy/api/utils/errors"
 )
 
-var (
-	productsDB = make(map[uint]*Product)
-)
-
 // Get - product
 func (p *Product) Get() *errors.ApiErr {
-	// https://gorm.io/ja_JP/docs/generic_interface.html
-	if err := products_db.Client.DB().Ping(); err != nil {
-		log.Fatal(err)
+	if result := products_db.Client.Where("id = ?", p.Model.ID).Find(&p); result.Error != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to get product: %s", result.GetErrors()),
+		)
 	}
-
-	result := productsDB[p.ID]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("product %d not found", p.ID))
-	}
-
-	p.ID = result.ID
-	p.Name = result.Name
-	p.Detail = result.Detail
-	p.Price = result.Price
-	p.Img = result.Img
-	p.CreatedAt = result.CreatedAt
-	p.UpdatedAt = result.UpdatedAt
-	p.DeletedAt = result.DeletedAt
-
 	return nil
 }
 
